9587993af38415bfd0c788ff5bd5d3edd30f167f
Emmanuel Ferte <eferte.omnilog@tf1.fr>
2021-06-23 16:51:30 +0200

    build

--- client/build/static/js/13.9504c25c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[13],{874:function(t,e,c){"use strict";c.r(e);var i=c(882),n=c(3);e.default=function(){var t=Object(i.a)().t;return Object(n.jsx)("div",{children:t("About-page-title")})}}}]);
-//# sourceMappingURL=13.9504c25c.chunk.js.map
