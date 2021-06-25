407e2a536aafa9e222bb40b694d0302f8ef886a4
Emmanuel Ferte <eferte.omnilog@tf1.fr>
2021-06-25 17:06:47 +0200

    improve ui

--- client/src/business/scheduler/page/SchedulesAll.tsx
+++ client/src/business/scheduler/page/SchedulesAll.tsx
@@ -4,16 +4,14 @@ import { useTranslation } from "react-i18next";
 import Panel from "_common/component/layout/panel/Panel";
 import endOfDay from "date-fns/endOfDay";
 import { clear, load } from "_common/service/SessionStorageService";
-import startOfDay from "date-fns/startOfDay";
-import format from "date-fns/format";
 
 const SchedulesAll = () => {
   const { t } = useTranslation();
   const urlParams = new URLSearchParams(window.location.search);
   const schedulerName = urlParams.get("schedulerName") || load("allSchedulerName", undefined);
   const scheduleId = urlParams.get("scheduleId") || load("allScheduleId", undefined);
-  const epochFrom = urlParams.get("epochFrom") || load("allEpochFrom",  format(startOfDay(new Date()), t("Calendar-date-format")));
-  const epochTo = urlParams.get("epochTo") || load("allEpochTo", format(endOfDay(new Date()), t("Calendar-date-format")));
+  const epochFrom = urlParams.get("epochFrom") || load("allEpochFrom",  undefined);
+  const epochTo = urlParams.get("epochTo") || load("allEpochTo", undefined);
   clear( (key) => {
     return key.indexOf("all") ===0;
   });
